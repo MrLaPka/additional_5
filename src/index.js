869c2e90dca36116e5708ba0070cb75449ab7900
Егор <42974925+MrLaPka@users.noreply.github.com>
2018-09-30 15:10:56 +0300

    6th commit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-    let boolean1 = false;
-    let increment1 = 0;
-    let increment2 = 0;
-    let increment3 = 0;
-    let increment4 = 0;
-    let arr = str.split("");
-    for(let i = 0;i<bracketsConfig.length;i++){
-        for(let j = 0;j<2;j++){
-        if(bracketsConfig[i][j] == '(')
-        increment1++;
-        if(bracketsConfig[i][j] == '{')
-        increment2++;
-        if(bracketsConfig[i][j] == '[')
-        increment3++;
-        if(bracketsConfig[i][j] == '|')
-        increment4++;
+
+let Stack = new Array;
+
+for(let i = 0; i < str.length; i++){
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        if(str[i] == bracketsConfig[j][0]  && bracketsConfig[j][0] == bracketsConfig[j][1] && bracketsConfig[j][0] == Stack[Stack.length - 1]){
+            Stack.pop();
+            break;
         }
-     }
-   for(let i = 0;i<arr.length;i++){
-      if(increment1>0){
-      if(((arr[i] == '(')&&(arr[arr.length - 1 - i] ==')'))||((arr[i] == '('))&&(arr[i+1] == '('))
-      boolean1 = true;
-      }
-      if(increment2>0){
-      if(((arr[i] == '{')&&(arr[arr.length - 1 - i] =='}'))||((arr[i] == '{'))&&(arr[i+1] == '}'))
-      boolean1 = true;
-      }
-      if(increment3>0){
-      if(((arr[i] == '[')&&(arr[arr.length - 1 - i] ==']'))||((arr[i] == '['))&&(arr[i+1] == ']'))
-      boolean1 = true;
-      }
-      if(increment4>0){
-      if(((arr[i] == '|')&&(arr[arr.length - 1 - i] =='|'))||((arr[i] == '|'))&&(arr[i+1] == '|'))
-      boolean1 = true;
-      }
-   }
-   return boolean1;// your solution
+
+        if (str[i] == bracketsConfig[j][0]){
+            Stack.push(str[i]);
+            break;
+        }
+
+        if((str[i] == bracketsConfig[j][1])&&(bracketsConfig[j][0] != Stack.pop())){
+            return false;
+        }
+    }
+}
+
+if(!Stack.pop()) return true;
+else
+return false;
 }
